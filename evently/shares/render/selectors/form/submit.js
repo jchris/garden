c3bd36799ff21948ba3e0ed9e9e4e2b0bfd45cf5
Chris Anderson <jchris@apache.org>
2010-04-05 15:48:27 -0700

    install progress

--- evently/shares/render/selectors/form/submit.js
+++ evently/shares/render/selectors/form/submit.js
@@ -4,18 +4,19 @@ function(e) {
   var db = $("select",this).val();
   var shared_doc_id = $("input[name=shared_doc_id]",this).val();
   var garden = app.require("lib/garden");
-
+  var form = this;
   function installApp() {
+    $(form).append('<p class="progress">Installing into ' + db + '...</p>');
     garden.installApp(app, db, shared_doc_id, {
       success : function() {
-        $.log("installed!")
+        $(".progress:last",form).text("Installed into "+db);
       }
     });
   }
 
   if (db == "_other") {
-    db = $("[name=other]", this).val();
-    var create = $("[name=create]", this).is(':checked');
+    db = $("[name=other]", form).val();
+    var create = $("[name=create]", form).is(':checked');
     if (create) {
       $.couch.db(db).create({
         success : installApp,
